87c061359dad1b914652f39bc8802e68df41c464
JuliandsonBarros <juliandsonbarros@gmail.com>
2023-05-23 16:47:09 -0300

    Primeiro commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './styles.css';
+
+import logoImage from '../../assets/logo.svg';
+import  padlock from'../../assets/padlock.png'
+
+
+export default function Login(){
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="erudio logo"/>
+                <form>
+                    <h1>Acesse sua Conta</h1>
+                    <input placeholder="username"/>
+                    <input placeholder="password"/>
+                </form>
+            </section>
+
+            <img src={padlock} alt="Login"/>
+        </div>
+    );
+}
